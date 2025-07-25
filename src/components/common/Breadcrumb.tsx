23f78c3754b416ea5322c7b281ce9146765cb552
Seonjin <ebngk0@gmail.com>
2025-07-25 13:42:35 +0900

    💄 style: 브레드크럼 디자인 일부 수정

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -6,19 +6,21 @@ interface BreadcrumbProps {
 
 export default function Breadcrumb({ items }: BreadcrumbProps) {
   return (
-    <nav className="breadcrumb mb-2 text-sm text-gray-600 max-w-[800px] mx-auto">
-      {items.map((item, index) => (
-        <span key={index}>
-          {item.href ? (
-            <Link href={item.href} className="hover:underline">
-              {item.label}
-            </Link>
-          ) : (
-            <span>{item.label}</span>
-          )}
-          {index < items.length - 1 && <span className="mx-2">{'>'}</span>}
-        </span>
-      ))}
+    <nav aria-label="Breadcrumb">
+      <ol className="text-xs lg:text-sm text-gray-400 flex flex-row flex-nowrap gap-2">
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.href ? (
+              <Link href={item.href} className="hover:underline">
+                {item.label}
+              </Link>
+            ) : (
+              <span>{item.label}</span>
+            )}
+            {index < items.length - 1 && <span className="mx-2">{'>'}</span>}
+          </li>
+        ))}
+      </ol>
     </nav>
   );
 }
